1af44acebd1d8571f6153df61946857e7cf32154
Andrew Kuchling <amk@amk.ca>
2015-04-11 09:34:17 -0400

    Update e-mail address; remove obsolete web page

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(name = 'texlib', version = '0.01',
       description = ("A package of Python modules for dealing with "
                      "various TeX-related file formats."),
       author = 'A.M. Kuchling',
-      author_email = 'akuchlin@mems-exchange.org',
-      url = "http://www.amk.ca/python/tex_wrap.html",
+      author_email = 'amk@amk.ca',
 
       packages = ['texlib'],
 )
